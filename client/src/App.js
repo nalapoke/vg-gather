22e83853c4895b2ce2e32a6711304c3efab8f7ea
nalapoke <09poqnat@gmail.com>
2022-01-09 16:14:19 -0600

    Make components use jsx extension and PascalCase

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
  
-import GameDetail from "./components/gameDetail";
-import Home from "./components/home";
+import GameDetail from "./components/GameDetail";
+import Home from "./components/Home";
  
 const App = () => {
  return (

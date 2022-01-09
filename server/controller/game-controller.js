0f134c22b30ed3f6b2a89a8050535de4bf9d8e4b
nalapoke <09poqnat@gmail.com>
2022-01-08 19:38:02 -0600

    Add get game by id endpoint. Make search use POST

--- server/controller/game-controller.js
+++ server/controller/game-controller.js
@@ -4,8 +4,11 @@ const twitchAuthenticator = require('../lib/twitch-authenticator')
 const clientId = process.env.TWITCH_CLIENT_ID
 
 searchGames = async (req, res) => {
-  if (!req.query.q) {
-    return res.status(400).json({ success: false, error: "Missing required query parameter 'q'" })
+  if (!req.body) {
+    return res.status(400).json({ success: false, error: "Missing required request body" })
+  }
+  if (!req.body.q) {
+    return res.status(400).json({ success: false, error: "Request body must provide a search query 'q'" })
   }
 
   try {
@@ -16,7 +19,7 @@ searchGames = async (req, res) => {
     const result = await client
       .limit(10)
       .fields(['id', 'name', 'first_release_date'])
-      .search(req.query.q)
+      .search(req.body.q)
       .request('/games') // execute the query and return a response object
 
     res.status(200).json({ success: true, data: result.data })
@@ -27,6 +30,31 @@ searchGames = async (req, res) => {
   }
 }
 
+getGameById = async (req, res) => {
+  try {
+    const accessToken = await twitchAuthenticator.getAccessCode()
+
+    const client = igdb(clientId, accessToken)
+
+    const result = await client
+      .fields(['name', 'summary', 'platforms.name', 'release_dates.human', 'screenshots.url'])
+      .where(`id = ${req.params.id}`)
+      .request('/games')
+
+    console.log(result.data)
+    if (!result.data || result.data.length == 0) {
+      return res.status(404).json({ success: false, error: "Game not found for id" })
+    }
+
+    res.status(200).json({ success: true, data: result.data })
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ success: false, error: "Oops. Something went wrong!" })
+  }
+}
+
 module.exports = {
-  searchGames
+  searchGames,
+  getGameById
 }
--- server/route/game-router.js
+++ server/route/game-router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const gameController = require('../controller/game-controller')
 const router = express.Router()
 
-router.get('/games/search', gameController.searchGames)
+router.post('/games/search', gameController.searchGames)
+router.get('/games/:id', gameController.getGameById)
 
 module.exports = router

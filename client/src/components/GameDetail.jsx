5fdbc2b75b235c915ae5a616d92f15f765abb7f6
nalapoke <09poqnat@gmail.com>
2022-01-09 19:31:13 -0600

    Show game cover on game detail

--- client/src/components/GameDetail.jsx
+++ client/src/components/GameDetail.jsx
@@ -1,8 +1,14 @@
+function buildCoverUrl(defaultCoverUrl) {
+  return defaultCoverUrl.replace('t_thumb', 't_cover_big').replace('//', 'https://');
+}
+
 export default function GameDetail(props) {
+  const coverUrl = buildCoverUrl(props.gameData.cover.url);
   return (
     <ul>
       <li>Name: {props.gameData.name}</li>
       <li>Summary: {props.gameData.summary}</li>
+      <li><img alt='cover' src={coverUrl} /></li>
     </ul>
   );
 }

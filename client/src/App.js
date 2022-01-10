20fe04700f0ba7d71b396d95db5c4f6e2460d3d7
nalapoke <09poqnat@gmail.com>
2022-01-09 21:53:35 -0600

    Beginnings of a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Navbar from 'react-bootstrap/Navbar';
  
 import Game from "./components/Game";
 import Home from "./components/Home";
  
 export default function App() {
   return (
-    <div>
+    <div className="app">
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/game/:id" element={<Game />} />
